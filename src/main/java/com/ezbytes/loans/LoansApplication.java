1a18fe01224e91ab60a62592d42505996d08f4d9
duongph <duongph@smartosc.com>
2022-11-07 07:45:03 +0700

    [Add] [Refresh scope] add annotation refresh scope

--- src/main/java/com/ezbytes/loans/LoansApplication.java
+++ src/main/java/com/ezbytes/loans/LoansApplication.java
@@ -2,8 +2,10 @@ package com.ezbytes.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class LoansApplication {
 
 	public static void main(String[] args) {
